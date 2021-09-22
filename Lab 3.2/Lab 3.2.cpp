db3a149538dc21386291ef514904dd406691af88
MarshmallowSoup <lysetskyyv@gmail.com>
2021-09-22 21:18:53 +0300

    first master commit

---
+++ Lab 3.2/Lab 3.2.cpp
@@ -0,0 +1,42 @@
+﻿// Лабораторна 3.2
+// Лисецький Володимир
+// Варіант 12
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	double x, a, b, c, F;
+	cout << "a = "; cin >> a;
+	cout << "b = "; cin >> b;
+	cout << "c = "; cin >> c;
+	cout << "x = "; cin >> x;
+
+	// Спосіб 1. Неповне розгалуження
+	if (x < 0.6 && b + c != 0)
+		F = (a * x * x) + (b * b) + c;
+	if (x > 0.6 && b + c == 0)
+		F = (x - a) / (x - c);
+	else
+		F = x / c - x / a;
+
+	cout << endl;
+	cout << "1) F = " << F << endl;
+
+	// Спосіб 2. Повне розгалуження 
+	if (x < 0.6 && b + c != 0)
+		F = (a * x * x) + (b * b) + c;
+	else
+		if (x > 0.6 && b + c == 0)
+			F = (x - a) / (x - c);
+		else
+			F = x / c - x / a;
+	
+	cout << "2) F = " << F << endl;
+	cin.get();
+
+	return 0;
+}
+
